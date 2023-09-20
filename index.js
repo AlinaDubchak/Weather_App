4e8bc183811f7dc5a82eaec8a7b6c2b8c497dcaf
alinadubchak <alina.dubchak.ev@gmail.com>
2023-09-20 09:28:58 +0300

    decompose if-else code

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ let windDirection = document.getElementById("direction");
 let weatherH1 = document.getElementById("weather-h1");
 let curLocation = document.querySelector(".curLocation");
 let icon = document.getElementById("img");
+let weatherTemp = document.querySelector(".inline");
 
 let apiKey = "001bc651977f4b024af4d84282b0f02a";
 let apiUrl = "https://api.openweathermap.org/data/2.5/weather?";
@@ -45,7 +46,7 @@ function date() {
   day.innerHTML = `${days[date.getDay()]} ${
     months[date.getMonth()]
   } ${date.getDate()}  ${date.getFullYear()}`;
-  if (date.getMinutes() < 10 || date.getHours() < 10) {
+  if (date.getMinutes() < 10 && date.getHours() < 10) {
     time.innerHTML = `${date.getHours()}:0${date.getMinutes()}`;
   } else {
     time.innerHTML = `${date.getHours()}:${date.getMinutes()}`;
@@ -53,6 +54,64 @@ function date() {
   navigator.geolocation.getCurrentPosition(handlePosition);
 }
 date();
+
+function showWeather(response) {
+  console.log(response);
+  icon.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  icon.setAttribute(
+    "alt",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].description}@2x.png`
+  );
+  city.innerHTML = `${response.data.name}`;
+  weatherTemp.innerHTML = `${Math.round(response.data.main.temp)} ℃`;
+  windSpeed.innerHTML = `${response.data.wind.speed} m/sec`;
+  weatherH1.innerHTML = `${response.data.weather[0].main}`;
+
+  let windDeg = response.data.wind.deg;
+  const windDirectionEl = getWindDirection(windDeg);
+  windDirection.innerHTML = windDirectionEl;
+}
+
+function getWindDirection(deg) {
+  const directions = [
+    "North",
+    "North-East",
+    "East",
+    "South-East",
+    "South",
+    "South-West",
+    "West",
+    "North-West",
+  ];
+  const index = Math.floor(((deg + 22.5) % 360) / 45);
+  return directions[index];
+}
+
+function handlePosition(position) {
+  let lat = position.coords.latitude;
+  let lon = position.coords.longitude;
+  axios
+    .get(`${apiUrl}lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`)
+    .then(showWeather);
+}
+
+curLocation.addEventListener("click", function (event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(handlePosition);
+});
+
+farenheit.addEventListener("click", (event) => {
+  event.preventDefault();
+  inline.innerHTML = `${Math.floor((response.data.main.temp * 9) / 5 + 32)} ℉`;
+});
+
+celcius.addEventListener("click", (event) => {
+  event.preventDefault();
+  inline.innerHTML = `${Math.round(response.data.main.temp)} ℃`;
+});
 
 formSearch.addEventListener("submit", function (event) {
   event.preventDefault();
@@ -70,64 +129,4 @@ searchButton.addEventListener("click", function (event) {
   axios
     .get(`${apiUrl}q=${city.innerHTML}&appid=${apiKey}&units=metric`)
     .then(showWeather);
-});
-
-let weatherTemp = document.querySelector(".inline");
-function showWeather(response) {
-  console.log(response);
-  icon.setAttribute(
-    "src",
-    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
-  );
-  icon.setAttribute(
-    "alt",
-    `http://openweathermap.org/img/wn/${response.data.weather[0].description}@2x.png`
-  );
-  city.innerHTML = `${response.data.name}`;
-  weatherTemp.innerHTML = `${Math.round(response.data.main.temp)} ℃`;
-  windSpeed.innerHTML = `${response.data.wind.speed} m/sec`;
-  weatherH1.innerHTML = `${response.data.weather[0].main}`;
-
-  let windDeg = response.data.wind.deg;
-  if (windDeg === 0 && windDeg === 360) {
-    windDirection.innerHTML = `North`;
-  } else if (windDeg === 90) {
-    windDirection.innerHTML = `East`;
-  } else if (windDeg === 180) {
-    windDirection.innerHTML = `South`;
-  } else if (windDeg === 270) {
-    windDirection.innerHTML = `West`;
-  } else if (windDeg > 0 && windDeg < 90) {
-    windDirection.innerHTML = `North-East`;
-  } else if (windDeg > 90 && windDeg < 180) {
-    windDirection.innerHTML = `South-East`;
-  } else if (windDeg > 180 && windDeg < 270) {
-    windDirection.innerHTML = `South-West`;
-  } else if (windDeg > 270 && windDeg < 360) {
-    windDirection.innerHTML = `North-West`;
-  }
-
-  farenheit.addEventListener("click", (event) => {
-    event.preventDefault();
-    inline.innerHTML = `${Math.floor(
-      (response.data.main.temp * 9) / 5 + 32
-    )} ℉`;
-  });
-  celcius.addEventListener("click", (event) => {
-    event.preventDefault();
-    inline.innerHTML = `${Math.round(response.data.main.temp)} ℃`;
-  });
-}
-
-function handlePosition(position) {
-  let lat = position.coords.latitude;
-  let lon = position.coords.longitude;
-  axios
-    .get(`${apiUrl}lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`)
-    .then(showWeather);
-}
-
-curLocation.addEventListener("click", function (event) {
-  event.preventDefault();
-  navigator.geolocation.getCurrentPosition(handlePosition);
 });
